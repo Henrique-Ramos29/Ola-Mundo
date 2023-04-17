fa583d3f30df223631364a2f1383f6be6167fd28
Henrique-Ramos29 <120992364+Henrique-Ramos29@users.noreply.github.com>
2023-04-16 21:37:05 -0300

    .

---
+++ src/paginas/Post/index.js
@@ -0,0 +1,31 @@
+import './Post.css'
+
+import { useParams } from "react-router-dom"
+import posts from "json/posts.json"
+import PostModelo from "componentes/PostModelo";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+
+export default function Post() {
+
+    const parametros = useParams();
+
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id)
+    })
+
+    return (
+        <PostModelo 
+            fotCapa={`/posts/${post.id}/capa.png`}
+            titulo={post.titulo}>
+                
+                <div classeName="post-markdown-container">
+
+                 <ReactMarkdown>
+                   {post.texto}
+                 </ReactMarkdown>
+
+                </div>
+        </PostModelo>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,31 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./paginas/Inicio"
-import SobreMim from  "./paginas/SobreMim"
+import SobreMim from "./paginas/SobreMim"
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-     <Menu /> 
-         <Routes>
-          <Route path="/" element={<PaginaPadrao />}>
-           <Route path="/" element={<Inicio />} />
-           <Route path="/sobremim" element={<SobreMim />} />
-         </Route>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+          <Route path="/post/:id" element={<Post />} />
+        </Route>
 
-          <Route path="*" element={<div>Pagina não encontrada</div>} />
-        </Routes>
+        <Route path="*" element={<div>Pagina não encontrada</div>} />
+      </Routes>
 
-     <Rodape />
+      <Rodape />
     </BrowserRouter>
-    
+
   );
-  
+
 };
 
 export default AppRoutes;
